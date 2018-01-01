a6f93f631558400aa7518e9598b536a0ecafbd45
PT_ <petertillema@solcon.nl>
2018-01-01 14:01:42 +0100

    Set dark pink background of all buildings

--- build.bat
+++ build.bat
@@ -9,19 +9,27 @@ if exist relocation_table2.asm del "relocation_table2.asm"
 cd gfx
 spasm -E -L appvar1.asm ..\bin\AOCEGFX1.8xv
 spasm -E -L appvar2.asm ..\bin\AOCEGFX2.8xv
-spasm -E -L aa1.asm ..\bin\AGE1.8xv
-spasm -E -L aa2.asm ..\bin\AGE2.8xv
-spasm -E -L aa3.asm ..\bin\AGE3.8xv
-spasm -E -L aa4.asm ..\bin\AGE4.8xv
+spasm -E -L age_1.asm ..\bin\AGE1.8xv
+spasm -E -L age_2.asm ..\bin\AGE2.8xv
+spasm -E -L age_3.asm ..\bin\AGE3.8xv
+spasm -E -L age_4.asm ..\bin\AGE4.8xv
 cd ..\bin
 call :editFile AOCEGFX1.lab
 call :editFile AOCEGFX2.lab
+call :editFile AGE1.lab
+call :editFile AGE2.lab
+call :editFile AGE3.lab
+call :editFile AGE4.lab
 cd ..
 spasm -E -T -L aoce.asm bin\aoce.bin
 convhex -x bin\aoce.bin
 del bin\aoce.bin
 del bin\AOCEGFX1.lab
 del bin\AOCEGFX2.lab
+del bin\AGE1.lab
+del bin\AGE2.lab
+del bin\AGE3.lab
+del bin\AGE4.lab
 pause
 exit
 
--- gfx/aa2.asm
+++
@@ -1,3 +0,0 @@
-#define db .db
-#define equ .equ
-#include "age_2.inc"
